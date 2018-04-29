c4ae0429ebdd9c25ff3efafc3a3795598681a8f7
Jacky <jacky8595125@gmail.com>
2018-04-30 00:57:52 +0800

    project init

---
+++ src/containers/Full.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <Navbar :narbarList="narbarList" />
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { Header as AppHeader } from '../components/'
+import Navbar from '../components/Navbar'
+
+export default {
+  name: 'full',
+  components: {
+    Navbar
+  },
+  data () {
+    return {
+
+      narbarList: [
+        {
+          href: '#/',
+          hrefName: 'HelloWorld'
+        },
+        {
+          hrefName: 'FooTable',
+          childrens: [
+            {
+              href: '#/fooTable',
+              hrefName: 'FooTable'
+            },
+            {
+              href: '#/fooTable2',
+              hrefName: 'FooTable2'
+            }
+          ]
+        }
+
+      ]
+
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+<style>
+@import 'footable/css/footable.standalone.min.css';
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
+
+
---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// Containers
+import Full from '@/containers/Full'
+
+// Views
+import HelloWorld from '@/components/HelloWorld'
+import FooTable from '@/components/FooTable'
+import FooTable2 from '@/components/FooTable2'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      redirect: '/dashboard',
+      name: 'Home',
+      component: Full,
+      children: [
+        {
+          path: 'dashboard',
+          name: 'Dashboard',
+          component: HelloWorld
+        },
+        {
+          path: 'footable',
+          name: 'FooTable',
+          component: FooTable
+        },
+        {
+          path: 'footable2',
+          name: 'FooTable2',
+          component: FooTable2
+        }
+      ]
+    }
+
+  ]
+})
