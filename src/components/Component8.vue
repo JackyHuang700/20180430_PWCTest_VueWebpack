67eca1e699a33e6c04cab7cee3e327e7b4bf5128
Jacky <jacky8595125@gmail.com>
2018-08-17 17:58:27 +0800

    ....

--- src/components/Component8.vue
+++ src/components/Component8.vue
@@ -1,14 +1,106 @@
 <template>
   <div class="conatiner-fluid">
     <div class="row">
-      <div class="col-3">
+      <section class="col-md-3">
         <h3>InfoCard</h3>
         <div>
-          <div class="wrap">
-
+          <div class="card0 cardRWD">
+            <div class="block-text">
+              <h2>
+                <span>標題asdfas</span>
+              </h2>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores numquam voluptatem nulla odit, nam quidem natus minus asperiores vero placeat nesciunt iure earum laborum! Nesciunt consectetur neque enim blanditiis, praesentium assumenda, eaque repudiandae iure molestias fugit ex rem quis id suscipit voluptates voluptatibus. Ut totam provident expedita laudantium suscipit ex quaerat aut! Incidunt similique perspiciatis facilis ipsa architecto praesentium dolores esse? Reprehenderit consequatur vitae reiciendis sapiente esse a fugiat tenetur neque ad harum atque aliquid ab omnis, totam obcaecati nostrum? Molestiae, aperiam beatae. Aliquid totam vero atque adipisci explicabo fuga minus, nobis debitis labore itaque rerum ducimus possimus quibusdam animi.</p>
+            </div>
+            <div class="block-date">
+              <div class="block-date-bg">
+                <span class="percent-75 dark"></span>
+                <span class="percent-25 light"></span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div>
+          <div class="card0 cardRWD">
+            <div class="block-text">
+              <h2>
+                <span></span>
+                標題asdfas
+              </h2>
+              <hr>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores numquam voluptatem nulla odit, nam quidem natus minus asperiores vero placeat nesciunt iure earum laborum! Nesciunt consectetur neque enim blanditiis, praesentium assumenda, eaque repudiandae iure molestias fugit ex rem quis id suscipit voluptates voluptatibus. Ut totam provident expedita laudantium suscipit ex quaerat aut! Incidunt similique perspiciatis facilis ipsa architecto praesentium dolores esse? Reprehenderit consequatur vitae reiciendis sapiente esse a fugiat tenetur neque ad harum atque aliquid ab omnis, totam obcaecati nostrum? Molestiae, aperiam beatae. Aliquid totam vero atque adipisci explicabo fuga minus, nobis debitis labore itaque rerum ducimus possimus quibusdam animi.</p>
+            </div>
+            <div class="block-date">
+              <div class="block-date-bg">
+                <span class="percent-75 dark"></span>
+                <span class="percent-25 light"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div>
+          <div class="card1">
+            <div class="header">
+              <h2>標題</h2>
+              <hr>
+            </div>
+            <div class="body">
+              sfaf
+            </div>
+            <div class="footer">
+              <div class="logo">三</div>
+              <span>footer</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div>
+          <div class="card1 card1Style2BorderTopColor">
+            <div class="header">
+              <h2>標題</h2>
+              <hr class="card1Style2BorderTopColor">
+            </div>
+            <div class="body">
+              sfaf
+            </div>
+            <div class="footer card1Style2BGC">
+              <div class="logo">三</div>
+              <span>footer</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card2">
+          <div class="header card2StyleBGC">Header</div>
+          <div class="body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad eum porro ratione eius nemo commodi doloremque officia voluptates necessitatibus aut nulla magnam dignissimos maiores, debitis voluptas vel iste cumque pariatur consequatur veniam ut deserunt minus? Cum, aut optio. Nam voluptatem esse reiciendis accusamus consectetur, totam, ipsum ut reprehenderit nesciunt minima excepturi ab aspernatur doloribus odit dolore velit expedita commodi temporibus porro ducimus beatae eaque cupiditate! Ab, ex error consequatur debitis perspiciatis ut omnis optio molestiae alias commodi ad accusamus blanditiis facilis quos itaque nobis explicabo quis minima vel quam aliquam. Aspernatur, natus deserunt rem pariatur dicta officia excepturi repudiandae quisquam.</div>
+          <div class="footer">Footer</div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card2">
+          <div class="header card2Style2BGC">Header</div>
+          <div class="body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad eum porro ratione eius nemo commodi doloremque officia voluptates necessitatibus aut nulla magnam dignissimos maiores, debitis voluptas vel iste cumque pariatur consequatur veniam ut deserunt minus? Cum, aut optio. Nam voluptatem esse reiciendis accusamus consectetur, totam, ipsum ut reprehenderit nesciunt minima excepturi ab aspernatur doloribus odit dolore velit expedita commodi temporibus porro ducimus beatae eaque cupiditate! Ab, ex error consequatur debitis perspiciatis ut omnis optio molestiae alias commodi ad accusamus blanditiis facilis quos itaque nobis explicabo quis minima vel quam aliquam. Aspernatur, natus deserunt rem pariatur dicta officia excepturi repudiandae quisquam.</div>
+          <div class="footer">Footer</div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card2">
+          <div class="header card2Style3BGC">Header</div>
+          <div class="body">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad eum porro ratione eius nemo commodi doloremque officia voluptates necessitatibus aut nulla magnam dignissimos maiores, debitis voluptas vel iste cumque pariatur consequatur veniam ut deserunt minus? Cum, aut optio. Nam voluptatem esse reiciendis accusamus consectetur, totam, ipsum ut reprehenderit nesciunt minima excepturi ab aspernatur doloribus odit dolore velit expedita commodi temporibus porro ducimus beatae eaque cupiditate! Ab, ex error consequatur debitis perspiciatis ut omnis optio molestiae alias commodi ad accusamus blanditiis facilis quos itaque nobis explicabo quis minima vel quam aliquam. Aspernatur, natus deserunt rem pariatur dicta officia excepturi repudiandae quisquam.</div>
+          <div class="footer">Footer</div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -23,5 +115,195 @@ export default {
   font-family: 'Noto Sans TC', sans-serif;
   padding: 0;
   margin: 0;
+}
+
+body {
+  background: #fff;
+  color: #999998;
+}
+
+/* 一 */
+.card0 {
+  background-color: #fff;
+  width: 350px;
+  height: auto;
+  box-shadow: 0 0 10px rgba(200, 201, 201, 0.5);
+}
+
+.card0 .block-text {
+  padding: 25px;
+  width: 100%;
+}
+
+.card0 .block-text h2 {
+  color: #b7aa9c;
+  font-style: 20px;
+  margin: 30px 0;
+  text-align: left;
+  position: relative;
+  width: 100%;
+}
+
+.card0 .block-text span {
+  border-bottom: 2px solid #efefef;
+}
+
+.card0 .block-text hr {
+  display: block;
+  border: none;
+  margin: 0;
+  width: 30px;
+  background-color: #efefef;
+  margin-bottom: 25px;
+  height: 2px;
+}
+
+.card0 .block-text p {
+  text-align: left;
+  font-weight: 300;
+  line-height: 20px;
+  font-size: 14px;
+}
+
+.card0 .block-date,
+.card0 .block-date-bg {
+  height: 10px;
+  width: 100%;
+}
+
+.card0 .block-date .percent-75 {
+  float: left;
+  height: inherit;
+  width: 75%;
+}
+.card0 .block-date .percent-25 {
+  float: left;
+  height: inherit;
+  width: 25%;
+}
+
+.card0 .block-date .dark {
+  background-color: #5fb2b2;
+}
+.card0 .block-date .light {
+  background-color: #9bcfcf;
+}
+
+/* 二 */
+.card1 {
+  border-color: #fff;
+  width: 350px;
+  height: auto;
+  box-shadow: 0 0 10px rgba(200, 201, 201, 0.5);
+  border-top: 15px solid #ff1643;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.card1 .header,
+.card1 .body {
+  padding: 15px;
+  text-align: left;
+}
+
+.card1 .header h2 {
+  margin-bottom: 10px;
+}
+
+.card1 .header hr {
+  display: block;
+  border: none;
+  margin: 0;
+  width: 30px;
+  background-color: #efefef;
+  margin-bottom: 25px;
+  height: 2px;
+  border: 2px solid red;
+}
+
+.card1 .body {
+  border: 0;
+}
+
+.card1 .footer {
+  display: inline-block;
+  width: 50%;
+  height: 20px;
+  line-height: 20px;
+  background-color: #ff1643;
+  margin-left: 50%;
+  border: 0;
+  /* border-top-left-radius: 10px; */
+  color: white;
+  text-align: left;
+  /* padding-left: 20px; */
+}
+
+.card1 .logo {
+  display: inline-block;
+  width: 20%;
+  text-align: center;
+  height: auto;
+}
+
+.card1Style2BGC.card1Style2BGC {
+  background-color: #1ce8b5;
+}
+
+.card1Style2BorderTopColor.card1Style2BorderTopColor.card1Style2BorderTopColor {
+  border-top-color: #1ce8b5;
+  border-color: #1ce8b5;
+}
+
+/* 三 */
+.card2 {
+  width: 100%;
+  max-width: 450px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px #dfe6f0;
+  text-align: left;
+}
+
+.card2 .header {
+  padding: 10px 20px;
+  min-height: 50px;
+  line-height: 50px;
+  height: auto;
+  text-align: center;
+  font-weight: bold;
+  font-size: 18px;
+  border-radius: 5px 5px 0 0;
+  color: white;
+  /* border: 1px solid red; */
+}
+
+.card2 .body {
+  color: #bdc5cd;
+  font-weight: 400;
+  padding: 10px 20px;
+  font-size: 14px;
+  letter-spacing: 1px;
+}
+
+.card2 .footer {
+  min-height: 50px;
+  line-height: 50px;
+  height: auto;
+  border-radius: 0 0 5px 5px;
+  /* border: 1px solid red; */
+  background-color: #f4f6f8;
+  color: #666;
+  font-size: 15px;
+  padding: 0 10px;
+}
+
+.card2StyleBGC {
+  background-color: #80ace9;
+}
+.card2Style2BGC {
+  background-color: #68CF8C;
+}
+.card2Style3BGC {
+  background-color: #ED7070;
 }
 </style>
