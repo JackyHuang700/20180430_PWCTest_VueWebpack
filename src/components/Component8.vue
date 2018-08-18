720143dcb313ad5f9bf14c4d92bed76c42cc71a9
Jacky <jacky8595125@gmail.com>
2018-08-18 15:51:48 +0800

    ....

--- src/components/Component8.vue
+++ src/components/Component8.vue
@@ -101,7 +101,30 @@
           <div class="footer">Footer</div>
         </div>
       </section>
-      <section class="col-md-3"></section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card3">
+          <div class="header card3StyleBGC">Header</div>
+          <div class="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, hic aliquid! Beatae facere ullam unde labore exercitationem incidunt earum necessitatibus ea eveniet quaerat neque excepturi veniam et consectetur at, distinctio odio eum eaque nemo quisquam eligendi magnam, praesentium alias. Ratione nobis porro tempora expedita, cumque temporibus nihil asperiores harum nam?</div>
+          <div class="footer"></div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card3">
+          <div class="header card3Style2BGC">Header</div>
+          <div class="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, hic aliquid! Beatae facere ullam unde labore exercitationem incidunt earum necessitatibus ea eveniet quaerat neque excepturi veniam et consectetur at, distinctio odio eum eaque nemo quisquam eligendi magnam, praesentium alias. Ratione nobis porro tempora expedita, cumque temporibus nihil asperiores harum nam?</div>
+          <div class="footer"></div>
+        </div>
+      </section>
+      <section class="col-md-3">
+        <h3>InfoCard</h3>
+        <div class="card3">
+          <div class="header card3Style3BGC">Header</div>
+          <div class="body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, hic aliquid! Beatae facere ullam unde labore exercitationem incidunt earum necessitatibus ea eveniet quaerat neque excepturi veniam et consectetur at, distinctio odio eum eaque nemo quisquam eligendi magnam, praesentium alias. Ratione nobis porro tempora expedita, cumque temporibus nihil asperiores harum nam?</div>
+          <div class="footer"></div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
@@ -303,9 +326,55 @@ body {
   background-color: #80ace9;
 }
 .card2Style2BGC {
-  background-color: #68CF8C;
+  background-color: #68cf8c;
 }
 .card2Style3BGC {
-  background-color: #ED7070;
+  background-color: #ed7070;
+}
+
+/* 四 */
+.card3 {
+  height: auto;
+  width: 100%;
+  max-width: 600px;
+  position: relative;
+  box-shadow: 0px 5px 10px #dfe6f0;
+  border-radius: 10px;
+}
+
+.card3 .header {
+  min-height: 36px;
+  max-height: 80px;
+  height: auto;
+  line-height: 36px;
+  width: 350px;
+  max-width: 450px;
+  position: absolute;
+  left: -40px;
+  top: 30px;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+
+.card3 .body{
+  padding: 90px 15px 20px;
+  color: #bdc5cd;
+  text-align: left;
+}
+
+.card3StyleBGC {
+  background-color: #00b0ff;
+}
+
+.card3Style2BGC {
+  background-color: #80ace9;
+}
+
+.card3Style3BGC {
+  background-color: #68cf8c;
 }
 </style>
